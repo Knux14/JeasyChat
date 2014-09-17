aa5da4a21dfdc7179cf4a174cf7f81bbd58607ff
Nathan J <knux14@gmail.com>
2014-09-17 15:07:58 +0200

    Some enhancement for logging

--- src/main/java/eu/knux/jeasychat/ConsolePanel.java
+++ src/main/java/eu/knux/jeasychat/ConsolePanel.java
@@ -2,6 +2,13 @@ package eu.knux.jeasychat;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
 
 /**
  * @author Nathan J. <knux14@gmail.com>
@@ -25,6 +32,24 @@ public class ConsolePanel extends JPanel {
 
         add(pane, BorderLayout.SOUTH);
 
+    }
+
+    public void log(Level level, String text) {
+        Date f = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yy-HH:mm:ss");
+        String message = "[" + level.getLocalizedName() + " - " + df.format(f) + "] -> " + text;
+
+        // Writing to the console
+        jep.setText(jep.getText() + message + "\n" );
+
+        // Writing to the log file
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("jeasychat.log", true)));
+            out.println(message);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
